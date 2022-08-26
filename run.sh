0ea78ad43fa5f224dbfcda6077d6f6a94307095d
Tegridy <jarjak4@gmail.com>
2022-08-26 08:32:01 +0200

    Vault configs update

--- run.sh
+++
@@ -1,7 +0,0 @@
-sleep 1
-vault secrets disable secret
-vault secrets enable -path=secret kv
-vault write secret/application eureka.instance.hostname=localhost \ eureka.client.service-url.defaultZone=http://eureka:password@localhost:8761/eureka/
-vault write secret/user-read-service spring.datasource.url=jdbc:postgresql://localhost:5433/user_db \ spring.datasource.username=postgres \ spring.datasource.password=qwerty
-vault write secret/user-write-service spring.datasource.url=jdbc:postgresql://localhost:5432/user_db \ spring.datasource.username=postgres \ spring.datasource.password=qwerty \ spring.rabbitmq.host=localhost \ spring.rabbitmq.port=5672 \ spring.rabbitmq.username=guest \ spring.rabbitmq.password=guest
-vault write secret/hackathon-write-service spring.datasource.url=jdbc:postgresql://localhost:5434/hackathon_db \ spring.datasource.username=postgres \ spring.datasource.password=qwerty
